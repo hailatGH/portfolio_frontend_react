25869683cb0f1801bdd5773f9edd0c546605f706
hailatGH <hailat.alx@gmail.com>
2023-01-05 15:37:49 +0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import { ReactDOM } from "react";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
