d1c877d229747fedf4d42936d5b793b6929bb031
hailatGH <hailat.alx@gmail.com>
2023-01-10 11:33:13 +0300

    Higher order component added to home and about page and they are working fine

---
+++ src/wrapper/AppWrap.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { SocialMedia, NavigationsDots } from "../components";
+
+const AppWrap = (Component, idName, classNames) =>
+  function HOC() {
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+
+        <div className="app__wrapper app__flex">
+          <Component />
+          <div className="copyright">
+            <p className="p-text">@2020 MICAEL</p>
+            <p className="p-text">All rights reserved</p>
+          </div>
+        </div>
+
+        <NavigationsDots active={idName} />
+      </div>
+    );
+  };
+
+export default AppWrap;
